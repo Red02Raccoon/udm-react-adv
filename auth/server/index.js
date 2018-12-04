465fc5ef4b67c9dcb507a992d18bf7844ca9630d
redraccoon <red02raccoon@gmail.com>
2018-12-04 22:58:46 +0200

    feat: server - add database and jwt

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,23 +1,27 @@
-const express =require('express');
-const http =require('http');
-const bodyParser =require('body-parser');
-const morgan =require('morgan');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
-const router = require('./router');
+const router = require("./router");
 
+const mongoose = require("mongoose");
+
+// DB setup
+mongoose.connect(
+  "mongodb://localhost:auth/auth",
+  { useNewUrlParser: true }
+);
 // App setup
-
-// 1. add middlewares
-app.use(morgan('combined'));
-app.use(bodyParser.json({type: '*/*'}));
+// add middlewares
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
-
-//some comment
 
 // Server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 
 server.listen(port);
-console.log("my port is",port)
+console.log("my port is", port);
